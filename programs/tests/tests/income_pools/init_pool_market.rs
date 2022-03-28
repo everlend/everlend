32a897729704c26629cc6dcd657cf26ea4ad1a62
vkarnaukhov <Lbvjybr611561>
2022-03-28 16:38:25 +0700

    test(program): negative tests for income pool

--- programs/tests/tests/income_pools/init_pool_market.rs
+++ programs/tests/tests/income_pools/init_pool_market.rs
@@ -1,8 +1,14 @@
 #![cfg(feature = "test-bpf")]
 
-use crate::utils::*;
-use everlend_income_pools::state::AccountType;
+use solana_program::instruction::InstructionError;
 use solana_program_test::*;
+use solana_sdk::signer::Signer;
+use solana_sdk::transaction::{Transaction, TransactionError};
+
+use everlend_income_pools::instruction;
+use everlend_income_pools::state::AccountType;
+
+use crate::utils::*;
 
 #[tokio::test]
 async fn success() {
@@ -20,4 +26,44 @@ async fn success() {
     let pool_market = test_income_pool_market.get_data(&mut context).await;
 
     assert_eq!(pool_market.account_type, AccountType::IncomePoolMarket);
+}
+
+#[tokio::test]
+async fn fail_second_time_init() {
+    let mut context = presetup().await.0;
+
+    let general_pool_market = TestGeneralPoolMarket::new();
+    general_pool_market.init(&mut context).await.unwrap();
+
+    let test_income_pool_market = TestIncomePoolMarket::new();
+    test_income_pool_market
+        .init(&mut context, &general_pool_market)
+        .await
+        .unwrap();
+
+    let pool_market = test_income_pool_market.get_data(&mut context).await;
+
+    assert_eq!(pool_market.account_type, AccountType::IncomePoolMarket);
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction::init_pool_market(
+            &everlend_income_pools::id(),
+            &test_income_pool_market.keypair.pubkey(),
+            &test_income_pool_market.manager.pubkey(),
+            &general_pool_market.keypair.pubkey(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    assert_eq!(
+        context
+            .banks_client
+            .process_transaction(tx)
+            .await
+            .unwrap_err()
+            .unwrap(),
+        TransactionError::InstructionError(0, InstructionError::AccountAlreadyInitialized)
+    )
 }
--- programs/tests/tests/income_pools/withdraw.rs
+++ programs/tests/tests/income_pools/withdraw.rs
@@ -1,8 +1,15 @@
 #![cfg(feature = "test-bpf")]
 
-use crate::utils::*;
+use solana_program::instruction::InstructionError;
+use solana_program::pubkey::Pubkey;
 use solana_program_test::*;
 use solana_sdk::signer::Signer;
+use solana_sdk::transaction::{Transaction, TransactionError};
+
+use everlend_income_pools::instruction;
+use everlend_utils::EverlendError;
+
+use crate::utils::*;
 
 const TOKEN_AMOUNT: u64 = 123 * EXP;
 
@@ -69,5 +76,187 @@ async fn success() {
     assert_eq!(
         get_token_balance(&mut context, &test_general_pool.token_account.pubkey()).await,
         TOKEN_AMOUNT
+    );
+}
+
+#[tokio::test]
+async fn success_with_zero_balance() {
+    let (mut context, test_income_pool_market, test_income_pool, test_general_pool) = setup().await;
+
+    assert_eq!(
+        get_token_balance(&mut context, &test_income_pool.token_account.pubkey()).await,
+        TOKEN_AMOUNT
+    );
+
+    test_income_pool
+        .withdraw(&mut context, &test_income_pool_market, &test_general_pool)
+        .await
+        .unwrap();
+
+    assert_eq!(
+        get_token_balance(&mut context, &test_income_pool.token_account.pubkey()).await,
+        0
+    );
+
+    context.warp_to_slot(3).unwrap();
+
+    //todo fail on withdraw with zero balance?
+    test_income_pool
+        .withdraw(&mut context, &test_income_pool_market, &test_general_pool)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn fail_with_invalid_income_pool_market() {
+    let (mut context, _, test_income_pool, test_general_pool) = setup().await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction::withdraw(
+            &everlend_income_pools::id(),
+            &Pubkey::new_unique(),
+            &test_income_pool.pool_pubkey,
+            &test_income_pool.token_account.pubkey(),
+            &test_general_pool.pool_pubkey,
+            &test_general_pool.token_account.pubkey(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    assert_eq!(
+        context
+            .banks_client
+            .process_transaction(tx)
+            .await
+            .unwrap_err()
+            .unwrap(),
+        TransactionError::InstructionError(
+            0,
+            InstructionError::Custom(EverlendError::InvalidAccountOwner as u32),
+        )
+    );
+}
+
+#[tokio::test]
+async fn fail_with_invalid_income_pool() {
+    let (mut context, test_income_pool_market, test_income_pool, test_general_pool) = setup().await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction::withdraw(
+            &everlend_income_pools::id(),
+            &test_income_pool_market.keypair.pubkey(),
+            &Pubkey::new_unique(),
+            &test_income_pool.token_account.pubkey(),
+            &test_general_pool.pool_pubkey,
+            &test_general_pool.token_account.pubkey(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    assert_eq!(
+        context
+            .banks_client
+            .process_transaction(tx)
+            .await
+            .unwrap_err()
+            .unwrap(),
+        TransactionError::InstructionError(
+            0,
+            InstructionError::Custom(EverlendError::InvalidAccountOwner as u32),
+        )
+    );
+}
+
+#[tokio::test]
+async fn fail_with_invalid_income_token_account() {
+    let (mut context, test_income_pool_market, test_income_pool, test_general_pool) = setup().await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction::withdraw(
+            &everlend_income_pools::id(),
+            &test_income_pool_market.keypair.pubkey(),
+            &test_income_pool.pool_pubkey,
+            &Pubkey::new_unique(),
+            &test_general_pool.pool_pubkey,
+            &test_general_pool.token_account.pubkey(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    assert_eq!(
+        context
+            .banks_client
+            .process_transaction(tx)
+            .await
+            .unwrap_err()
+            .unwrap(),
+        TransactionError::InstructionError(0, InstructionError::InvalidArgument)
+    );
+}
+
+#[tokio::test]
+async fn fail_with_invalid_general_pool() {
+    let (mut context, test_income_pool_market, test_income_pool, test_general_pool) = setup().await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction::withdraw(
+            &everlend_income_pools::id(),
+            &test_income_pool_market.keypair.pubkey(),
+            &test_income_pool.pool_pubkey,
+            &test_income_pool.token_account.pubkey(),
+            &Pubkey::new_unique(),
+            &test_general_pool.token_account.pubkey(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    assert_eq!(
+        context
+            .banks_client
+            .process_transaction(tx)
+            .await
+            .unwrap_err()
+            .unwrap(),
+        TransactionError::InstructionError(
+            0,
+            InstructionError::Custom(EverlendError::InvalidAccountOwner as u32),
+        )
+    );
+}
+
+#[tokio::test]
+async fn fail_with_invalid_general_pool_token_account() {
+    let (mut context, test_income_pool_market, test_income_pool, test_general_pool) = setup().await;
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction::withdraw(
+            &everlend_income_pools::id(),
+            &test_income_pool_market.keypair.pubkey(),
+            &test_income_pool.pool_pubkey,
+            &test_income_pool.token_account.pubkey(),
+            &test_general_pool.pool_pubkey,
+            &Pubkey::new_unique(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    assert_eq!(
+        context
+            .banks_client
+            .process_transaction(tx)
+            .await
+            .unwrap_err()
+            .unwrap(),
+        TransactionError::InstructionError(0, InstructionError::InvalidArgument)
     );
 }
