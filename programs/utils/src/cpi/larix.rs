60c1327899f410603ed216fd7ed6f966541a63b6
Vladislav Karnaukhov <karnaukhov.vl@gmail.com>
2022-05-12 16:11:26 +0700

    feat(program): larix rewards collecting instructions

--- programs/utils/src/cpi/larix.rs
+++ programs/utils/src/cpi/larix.rs
@@ -1,3 +1,5 @@
+use larix_lending::instruction::LendingInstruction;
+use solana_program::instruction::{AccountMeta, Instruction};
 use solana_program::{
     account_info::AccountInfo,
     program::{invoke, invoke_signed},
@@ -5,7 +7,7 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
-pub fn refresh_reserve<'a>(
+pub fn refresh_reserves<'a>(
     program_id: &Pubkey,
     reserve: AccountInfo<'a>,
     reserve_liquidity_oracle: AccountInfo<'a>,
@@ -17,6 +19,37 @@ pub fn refresh_reserve<'a>(
     );
 
     invoke(&ix, &[reserve, reserve_liquidity_oracle])
+}
+
+pub fn refresh_mining<'a>(
+    program_id: &Pubkey,
+    mining_info: AccountInfo<'a>,
+    reserve: AccountInfo<'a>,
+) -> Result<(), ProgramError> {
+    let ix = Instruction {
+        program_id: *program_id,
+        accounts: vec![
+            AccountMeta::new_readonly(*mining_info.key, false),
+            AccountMeta::new_readonly(*reserve.key, false),
+        ],
+        data: LendingInstruction::RefreshMining.pack(),
+    };
+
+    invoke(&ix, &[mining_info, reserve])
+}
+
+pub fn refresh_obligation<'a>(
+    program_id: &Pubkey,
+    obligation_info: AccountInfo<'a>,
+    reserve: AccountInfo<'a>,
+) -> Result<(), ProgramError> {
+    let ix = larix_lending::instruction::refresh_obligation(
+        *program_id,
+        *obligation_info.key,
+        vec![*reserve.key],
+    );
+
+    invoke(&ix, &[obligation_info, reserve])
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -103,4 +136,122 @@ pub fn redeem<'a>(
         ],
         signers_seeds,
     )
+}
+
+pub fn init_mining<'a>(
+    program_id: &Pubkey,
+    // Random uninitialized lending program account for future Mining account
+    mining_info: AccountInfo<'a>,
+    authority: AccountInfo<'a>,
+    lending_market: AccountInfo<'a>,
+) -> Result<(), ProgramError> {
+    let ix = Instruction {
+        program_id: *program_id,
+        accounts: vec![
+            AccountMeta::new_readonly(*mining_info.key, false),
+            AccountMeta::new_readonly(*authority.key, true),
+            AccountMeta::new_readonly(*lending_market.key, false),
+        ],
+        data: LendingInstruction::InitMining.pack(),
+    };
+
+    invoke(&ix, &[mining_info, authority, lending_market])
+}
+
+pub fn deposit_mining<'a>(
+    program_id: &Pubkey,
+    destination_collateral: AccountInfo<'a>,
+    // Contains is reserve account ...bonus.unCollSupply
+    reserve_bonus: AccountInfo<'a>,
+    mining_info: AccountInfo<'a>,
+    reserve: AccountInfo<'a>,
+    lending_market: AccountInfo<'a>,
+    mining_owner: AccountInfo<'a>,
+    authority: AccountInfo<'a>,
+    // Use u64::MAX for depositing 100% of available amount
+    amount: u64,
+) -> Result<(), ProgramError> {
+    let ix = Instruction {
+        program_id: *program_id,
+        accounts: vec![
+            AccountMeta::new(*destination_collateral.key, false),
+            AccountMeta::new(*reserve_bonus.key, false),
+            AccountMeta::new(*mining_info.key, false),
+            AccountMeta::new_readonly(*reserve.key, false),
+            AccountMeta::new_readonly(*lending_market.key, false),
+            AccountMeta::new_readonly(*mining_owner.key, false),
+            AccountMeta::new_readonly(*authority.key, true),
+            AccountMeta::new_readonly(spl_token::id(), false),
+        ],
+        data: LendingInstruction::DepositMining { amount }.pack(),
+    };
+
+    invoke(
+        &ix,
+        &[
+            destination_collateral,
+            reserve_bonus,
+            mining_info,
+            reserve,
+            lending_market,
+            mining_owner,
+            authority,
+        ],
+    )
+}
+
+pub fn claim_mine<'a>(
+    program_id: &Pubkey,
+    destination_collateral: AccountInfo<'a>,
+    mining_info: AccountInfo<'a>,
+    obligation_info: Option<AccountInfo<'a>>,
+    reserve: AccountInfo<'a>,
+    lending_market: AccountInfo<'a>,
+    lending_market_authority: AccountInfo<'a>,
+    authority: AccountInfo<'a>,
+    // claim times of user expected got: 100 equals 100%
+    claim_times: u16,
+    // the ratio of claim user's all mine token 10000 equals 100%
+    claim_ratio: u16,
+) -> Result<(), ProgramError> {
+    const OBLIGATION_ACCOUNT_INDEX: usize = 5;
+
+    let mut accounts_meta = vec![
+        AccountMeta::new_readonly(spl_token::id(), false),
+        AccountMeta::new_readonly(*lending_market.key, false),
+        AccountMeta::new_readonly(*lending_market_authority.key, false),
+        AccountMeta::new_readonly(*authority.key, true),
+        AccountMeta::new(*mining_info.key, false),
+        AccountMeta::new(*destination_collateral.key, false),
+        AccountMeta::new_readonly(*reserve.key, false),
+    ];
+
+    let mut accounts = vec![
+        lending_market,
+        lending_market_authority,
+        authority,
+        mining_info,
+        destination_collateral,
+        reserve,
+    ];
+
+    if let Some(obligation_info) = obligation_info {
+        accounts_meta.insert(
+            OBLIGATION_ACCOUNT_INDEX,
+            AccountMeta::new_readonly(*obligation_info.key, false),
+        );
+        accounts.insert(OBLIGATION_ACCOUNT_INDEX, obligation_info);
+    }
+
+    let ix = Instruction {
+        program_id: *program_id,
+        accounts: accounts_meta,
+        data: LendingInstruction::ClaimMine {
+            claim_times,
+            claim_ratio,
+        }
+        .pack(),
+    };
+
+    invoke(&ix, &accounts)
 }
