b966b308775344600c4cc80c458c737ec6994982
Messer4 <mikle0497@gmail.com>
2022-05-25 17:57:51 +0300

    feat(income_fee): missed toolkit command matcher

--- programs/toolkit/src/main.rs
+++ programs/toolkit/src/main.rs
@@ -853,6 +853,25 @@ async fn main() -> anyhow::Result<()> {
                     ),
                 ),
         )
+        .subcommand(
+            SubCommand::with_name("create-safety-fund-token-account")
+                .about("Run  create income pool safety fund token account")
+                .arg(
+                    Arg::with_name("case")
+                        .value_name("NAME")
+                        .takes_value(true)
+                        .index(1)
+                        .help("Case"),
+                )
+                .arg(
+                    Arg::with_name("accounts")
+                        .short("A")
+                        .long("accounts")
+                        .value_name("PATH")
+                        .takes_value(true)
+                        .help("Accounts file"),
+                ),
+        )
         .get_matches();
 
     let mut wallet_manager = None;
